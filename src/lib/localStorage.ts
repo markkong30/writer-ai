d497b01b1c73744a4ea4009e9c1d7d4c6b915dc5
markkong30 <markkong30@gmail.com>
2023-04-12 00:42:30 +0100

    small fix

--- src/lib/localStorage.ts
+++ src/lib/localStorage.ts
@@ -2,13 +2,20 @@ export enum LocalStorageKeys {
   botWelcome = 'aai-bot-welcome',
 }
 
-export const setLocalStorage = <T>(key: LocalStorageKeys, value: T): void =>
-  window?.localStorage?.setItem(key, JSON.stringify(value));
+export const setLocalStorage = <T>(key: LocalStorageKeys, value: T): void => {
+  if (typeof window !== 'undefined') {
+    window?.localStorage?.setItem(key, JSON.stringify(value));
+  }
+};
 
 export const getLocalStorage = <T>(key: LocalStorageKeys): T | null => {
-  const value = window?.localStorage?.getItem(key);
-  if (value) {
-    return JSON.parse(value) as T;
+  if (typeof window !== 'undefined') {
+    const value = window?.localStorage?.getItem(key);
+    if (value) {
+      return JSON.parse(value) as T;
+    }
+    return null;
   }
+
   return null;
 };
